ac0cbfdc97998eb6a164888582186310da9bf01b
Prajit <prajitramesh123@gmail.com>
2026-02-01 21:14:56 +0530

    created template for other users

---
+++ src/app/socialmedia/page.tsx
@@ -0,0 +1,7 @@
+export default function SocialMediaPage() {
+    return (
+        <div className="min-h-screen bg-black flex items-center justify-center">
+            <h1 className="text-4xl font-black text-white/20 uppercase tracking-widest">Social Media</h1>
+        </div>
+    );
+}
